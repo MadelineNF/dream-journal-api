70f91eb9f7d4847117c67d5b87b4bb7237fffe3d
Madeline <madelinenelsonfolkersen@gmail.com>
2017-07-14 16:33:47 -0400

    I hate cors

--- config/application.rb
+++ config/application.rb
@@ -17,15 +17,16 @@ require "rails/test_unit/railtie"
 Bundler.require(*Rails.groups)
 module DreamJournalAPI
   class Application < Rails::Application
-   Rails.application.config.middleware.insert_before 0, Rack::Cors do
-     allow do
-       origins 'example.com'
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+  
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
 
-       resource '*',
-         headers: :any,
-         methods: [:get, :post, :put, :patch, :delete, :options, :head]
-     end
-    end
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
 end
