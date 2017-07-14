e77cb5c748357e89ce90c53176cebac82bdc8a96
Madeline <madelinenelsonfolkersen@gmail.com>
2017-07-14 14:13:00 -0400

    fixed CORS in application.rb

--- dreamJournalAPI/config/application.rb
+++ dreamJournalAPI/config/application.rb
@@ -15,15 +15,12 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => '*',
-    'Access-Control-Request-Method' => %w{GET POST PUT DELETE OPTIONS}.join(",")
-  }
 module DreamJournalAPI
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => '*'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
