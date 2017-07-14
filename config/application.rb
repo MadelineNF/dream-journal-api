d8b3acceed316cf2feea74df7eceb310ed828313
Madeline <madelinenelsonfolkersen@gmail.com>
2017-07-14 16:00:31 -0400

    still working on CORS *sigh*

--- config/application.rb
+++ config/application.rb
@@ -17,24 +17,15 @@ require "rails/test_unit/railtie"
 Bundler.require(*Rails.groups)
 module DreamJournalAPI
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-    Rails.application.config.middleware.insert_before 0, Rack::Cors do
-   allow do
-     origins 'example.com'
+   Rails.application.config.middleware.insert_before 0, Rack::Cors do
+     allow do
+       origins 'example.com'
 
-     resource '*',
-       headers: :any,
-       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-   end
- end
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+       resource '*',
+         headers: :any,
+         methods: [:get, :post, :put, :patch, :delete, :options, :head]
+     end
+    end
     config.api_only = true
   end
 end
