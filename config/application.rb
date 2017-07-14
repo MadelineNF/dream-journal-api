1804d0b4cc292d9e230e6499ecfd9d2a6b223be7
Madeline <madelinenelsonfolkersen@gmail.com>
2017-07-14 15:39:20 -0400

    switched CORS code for rails-cors code

--- config/application.rb
+++ config/application.rb
@@ -19,11 +19,12 @@ module DreamJournalAPI
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => %w{GET POST PUT DELETE OPTIONS}.join(","),
-      'X-Jorts' => 'hello'
-    }
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
