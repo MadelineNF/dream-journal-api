f3940a0dbf9b2d1183c942475c112835728d6eac
Madeline <madelinenelsonfolkersen@gmail.com>
2017-07-14 09:44:48 -0400

    added routes for entries and entries controller

---
+++ dreamJournalAPI/app/controllers/entries_controller.rb
@@ -0,0 +1,38 @@
+class EntriesController < ApplicationController
+  def index
+    @entries = Entry.order(:id)
+    render json: { message: "ok", entries_data: @entries }
+  end
+  
+  def show
+    begin
+      @entry = Entry.find(params[:id])
+      render json: { message: "ok", entries_data: @entry }
+    rescue ActiveRecord::RecordNotFound
+      render json: { message: "no entry matches that ID" }, status: 404
+    rescue Exception
+      render json: { message: "there was some other error" }, status: 500
+    end
+  end
+
+  def create
+    begin
+      @entry = Entry.new(entry_params)
+      @entry.save 
+      @all_entries = Entry.order(:id)
+      render json: { message: "entry added successfully", entries_data: @all_entries }
+    rescue Exception
+      render json: { message: "there was an error" }, status: 500
+    end
+  end
+
+  def update
+    begin 
+      @entry = Entry.find(params[:id])
+      @entry.update(entry_params)
+      @all_entries = Entry.order(:id)
+      render json: { message: "entry updated successfully", entries_data: @all_entries }
+    rescue Exception
+      render json: { message: "there was an error" }
+    end
+  end
